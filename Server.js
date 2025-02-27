dba0e791d37686a02fd09b084c2a3df988733bc9
Jayesh <jayeshjaiswal2510@gmail.com>
2025-02-27 22:37:47 +0530

    ..

--- Server.js
+++ Server.js
@@ -26,3 +26,5 @@ app.use("/api/expense", authMiddleware, expenseRoutes);
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
